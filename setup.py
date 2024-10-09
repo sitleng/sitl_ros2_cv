0c32cbfb953c163af6f213facae21d6a49dd342c
Ki-Hwan Oh <sktlgt93@gmail.com>
2024-10-09 14:33:05 -0500

    Minor Updates
    Change name of the project

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import os
 from glob import glob
 from setuptools import setup
 
-package_name = 'sitl_dvrk_ros2'
+package_name = 'sitl_ros2_cv'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=[package_name, 'utils', 'nodes'],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -16,23 +16,23 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='sitl-dvrk-sub',
+    maintainer='koh',
     maintainer_email='sktlgt93@gmail.com',
-    description='ROS2 Package for dVRK in SITL',
+    description='SITL ROS2 Package for Image Processing and Computer Vision Applications',
     license='Apache License 2.0',
     entry_points={
         'console_scripts': [
-            'pub_ecm_left_info  = sitl_dvrk_ros2.pub_ecm_left_info:main',
-            'pub_ecm_left_raw   = sitl_dvrk_ros2.pub_ecm_left_raw:main',
-            'pub_ecm_left_rect  = sitl_dvrk_ros2.pub_ecm_left_rect:main',
-            'pub_ecm_right_info = sitl_dvrk_ros2.pub_ecm_right_info:main',
-            'pub_ecm_right_raw  = sitl_dvrk_ros2.pub_ecm_right_raw:main',
-            'pub_ecm_right_rect = sitl_dvrk_ros2.pub_ecm_right_rect:main',
-            'pub_ecm_disp       = sitl_dvrk_ros2.pub_ecm_disp:main',
-            'pub_ecm_pclimg     = sitl_dvrk_ros2.pub_ecm_pclimg:main',
-            'pub_ecm_pcl        = sitl_dvrk_ros2.pub_ecm_pcl:main',
-            'pub_ecm_recon3d    = sitl_dvrk_ros2.pub_ecm_recon3d:main',
-            'image_view         = sitl_dvrk_ros2.image_view:main'
+            'pub_ecm_left_info  = sitl_ros2_cv.pub_ecm_left_info:main',
+            'pub_ecm_left_raw   = sitl_ros2_cv.pub_ecm_left_raw:main',
+            'pub_ecm_left_rect  = sitl_ros2_cv.pub_ecm_left_rect:main',
+            'pub_ecm_right_info = sitl_ros2_cv.pub_ecm_right_info:main',
+            'pub_ecm_right_raw  = sitl_ros2_cv.pub_ecm_right_raw:main',
+            'pub_ecm_right_rect = sitl_ros2_cv.pub_ecm_right_rect:main',
+            'pub_ecm_disp       = sitl_ros2_cv.pub_ecm_disp:main',
+            'pub_ecm_pclimg     = sitl_ros2_cv.pub_ecm_pclimg:main',
+            'pub_ecm_pcl        = sitl_ros2_cv.pub_ecm_pcl:main',
+            'pub_ecm_recon3d    = sitl_ros2_cv.pub_ecm_recon3d:main',
+            'image_view         = sitl_ros2_cv.image_view:main'
         ],
     },
 )
